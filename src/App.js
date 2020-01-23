47db838fbb86837ec9653c568181fd9960f3f914
MalihaKabir <malihakabir01@gmail.com>
2020-01-24 00:25:50 +0600

    cleaned unnecessary codes

--- src/App.js
+++ src/App.js
@@ -151,27 +151,24 @@ class App extends Component {
 					!this.state.imgURL && !this.state.imgForUpload ? <p className='f3'>
 						Hello! What are you waiting for?
 					</p> :
-					this.state.imgURL === this.state.inputField && !this.state.imgForUpload ? <FaceRecognitionFromURL
+					this.state.imgURL && !this.state.imgForUpload ? <FaceRecognitionFromURL
 						box={this.state.demography.box}
 						imgURL={this.state.imgURL}
 						demoGen={this.state.demography.gender}
 						demoAge={this.state.demography.age}
 					/> :
-					!this.state.imgURL &&
-					this.state.imgForUpload === this.state.selectedFile ? <FaceRecognitionFromBrowse
+					!this.state.imgURL && this.state.imgForUpload ? <FaceRecognitionFromBrowse
 						box={this.state.demography.box}
 						imgForUpload={this.state.imgForUpload}
 						demoGen={this.state.demography.gender}
 						demoAge={this.state.demography.age}
 					/> :
-					this.state.imgURL === this.state.inputField &&
-					this.state.imgForUpload ===
-						this.state
-							.selectedFile ? <p className='f3 pt4 ma4 lh-copy'>
-						{`Oops! I believe you've tried to detect in both ways at a time. I'm afraid you can select only one input at the same time. Kindly browse your desired photo from your device or grab a direct link to a file on the web and give it to us. We're always ready to detect it for you!`}
+					this.state.imgURL && this.state.imgForUpload ? <p className='f3 pt4 ma4 lh-copy'>
+						{`Oops! ${(
+							<br />
+						)} I believe you've tried to detect in both ways at a time. I'm afraid you can select only one input at the same time. Kindly browse your desired photo from your device or grab a direct link to a file on the web and give it to us. We're always ready to detect it for you!`}
 					</p> :
-					<p>{'Error occurred!'}</p>
-				}
+					<p>{'Error occurred!'}</p>}
 			</div>
 		);
 	}
